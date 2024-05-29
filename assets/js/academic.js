82d85e4ebecf7a4fb4b50b22410d638b14efdb29
Gunnar Voet <gvoet@ucsd.edu>
2024-05-29 13:08:37 +0200

    update map links to https

--- assets/js/academic.js
+++ assets/js/academic.js
@@ -267,7 +267,7 @@
         let map = new L.map('map').setView([lat, lng], zoom);
         if (map_provider == 3 && api_key.length) {
           L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            attribution: 'Map data &copy; <a href="https://openstreetmap.org">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://mapbox.com">Mapbox</a>',
             maxZoom: 18,
             id: 'mapbox.streets',
             accessToken: api_key
@@ -275,7 +275,7 @@
         } else {
           L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
           }).addTo(map);
         }
         let marker = L.marker([lat, lng]).addTo(map);
