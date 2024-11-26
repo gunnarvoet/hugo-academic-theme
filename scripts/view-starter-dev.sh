0112187157806c411d9cd6353957eb453ac17e0a
Jaeyoung Cho <joeyadamcho@gmail.com>
2024-11-26 13:13:04 +0100

    fix some bugs

--- scripts/view-starter-dev.sh
+++ scripts/view-starter-dev.sh
@@ -7,5 +7,5 @@ github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-reveal -> ../..
 github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-seo -> ../../../modules/wowchemy-seo,
 github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-core -> ../../../modules/wowchemy-core,
 github.com/wowchemy/wowchemy-hugo-themes/modules/wowchemy-plugin-netlify-cms -> ../../../modules/wowchemy-plugin-netlify-cms" \
-hugo server --panicOnWarning --renderStaticToDisk -F --port 8081 --bind 0.0.0.0 \
---source "starters/$1"
+hugo --panicOnWarning --renderStaticToDisk -F \
+--source "starters/$1" --destination "./build"
